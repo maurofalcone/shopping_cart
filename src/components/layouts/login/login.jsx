de2d405c5b470f74dc14d322807b7881968ee3f9
Mauro Falcone <mauro.falcone@radiumrocket.com>
2019-10-14 15:13:11 -0300

    footer structure on login layout

--- src/components/layouts/login/login.jsx
+++ src/components/layouts/login/login.jsx
@@ -7,6 +7,9 @@ const LoginLayout = ({children}) => {
       <div className={css.childrenContainer}>
         {children}
       </div>
+      <div className={css.footerContainer}>
+        Footer
+      </div>
     </div>
   );
 };
